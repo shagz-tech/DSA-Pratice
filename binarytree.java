fd989044cdd2889de9101a29e6b54f84d4aec5d2
shagun <shagung072@gmail.com>
2026-04-15 14:49:03 +0530

    commit

---
+++ binarytree.java
@@ -0,0 +1,24 @@
+public class binarytree {
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void postorder(Node root){
+        if(root == null){
+            return;
+        }
+    }
+    
+}
