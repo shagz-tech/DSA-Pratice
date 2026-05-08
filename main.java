86a047516058aed200794fabf6cea65d66c385d1
shagun <shagung072@gmail.com>
2026-05-08 13:34:18 +0530

    commit

---
+++ main.java
@@ -0,0 +1,22 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
